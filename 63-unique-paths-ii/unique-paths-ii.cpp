9b889578533e7a65f79ccb1e82f7fbaf6ac5fbc7
Naeem <126136788+MuhammadNaeem001@users.noreply.github.com>
2025-10-10 00:45:21 -0700

    Time: 0 ms (100.00%) | Memory: 12 MB (29.25%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<long long>> dp(m, vector<long long>(n, 0));
+
+        // If the starting cell has an obstacle, return 0
+        if (obstacleGrid[0][0] == 1) return 0;
+        dp[0][0] = 1;
+
+        // Fill the DP table
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; // obstacle = no path
+                } else {
+                    if (i > 0) dp[i][j] += dp[i - 1][j];
+                    if (j > 0) dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};

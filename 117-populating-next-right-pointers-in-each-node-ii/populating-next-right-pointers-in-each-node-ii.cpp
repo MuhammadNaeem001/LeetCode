9ac389300ca03b04609948bff9fe3cedad7991a8
Naeem <126136788+MuhammadNaeem001@users.noreply.github.com>
2025-10-11 21:56:32 -0700

    Time: 12 ms (29.24%) | Memory: 18.7 MB (91.93%) - LeetSync

---
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+
+        Node* curr = root;
+
+        while (curr) {
+            Node dummy(0);   // Dummy node for next level
+            Node* tail = &dummy;
+
+            while (curr) {
+                if (curr->left) {
+                    tail->next = curr->left;
+                    tail = tail->next;
+                }
+                if (curr->right) {
+                    tail->next = curr->right;
+                    tail = tail->next;
+                }
+                curr = curr->next; // move to next node in current level
+            }
+
+            curr = dummy.next; // move to first node of next level
+        }
+
+        return root;
+    }
+};
